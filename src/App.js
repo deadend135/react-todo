aea9486474daaedc9f740daf4e1fcb2fe0057f82
deadend135 <sharma.vikrant135@gmail.com>
2019-05-16 17:52:18 +0530

    to-do app till saga

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Suspense, lazy } from 'react';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//import AddToDo from './containers/AddToDo';
+//import ToDoListContainer from './containers/ToDoListContainer';
+
+const AddToDo = lazy(() => import('./containers/AddToDo'));
+// const AddToDo = lazy(() => {
+//   return new Promise(resolve => {
+//     setTimeout(() => resolve(import("./containers/AddToDo"), 300);
+//   });
+// });
+
+const ToDoListContainer = lazy(() => import('./containers/ToDoListContainer'));
+// const AddToDo = lazy(() => {
+//   return new Promise(resolve => {
+//     setTimeout(() => resolve(import("./containers/ToDoListContainer"), 300);
+//   });
+// });
+class App extends Component {
+  render() {
+    return (
+      <Container>
+        <Row className="row">
+          <Col xs={12}>
+            <h1>To Do List</h1>
+            <Suspense fallback={<div>loading .......</div>}>
+              <AddToDo />
+              <ToDoListContainer />
+            </Suspense>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default App;
